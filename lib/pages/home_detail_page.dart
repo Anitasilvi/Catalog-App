50fd28e5812eaf81b2abef506dd89178db836baa
anitasilvi <anitaferdina06@gmail.com>
2023-01-16 20:32:52 +0700

    Cart Page | FloatingActionButton | Dummy Text

--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -12,7 +12,9 @@ class HomeDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
       backgroundColor: MyTheme.creamColor,
       bottomNavigationBar: Container(
         color: Colors.white,
@@ -24,48 +26,52 @@ class HomeDetailPage extends StatelessWidget {
             ElevatedButton(
               onPressed: () {},
               style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                    MyTheme.darkBluishColor,
-                  ),
-                  shape: MaterialStateProperty.all(
-                    StadiumBorder(),
-                  )),
-              child: "Buy".text.make(),
-            ).wh(100, 50)
+                  backgroundColor:
+                      MaterialStateProperty.all(MyTheme.darkBluishColor),
+                  shape: MaterialStateProperty.all(StadiumBorder())),
+              child: "Add to cart".text.make(),
+            ).wh(120, 50)
           ],
         ).p32(),
       ),
       body: SafeArea(
-        bottom: false,
-        child: Column(
-          children: [
-            Hero(
-              tag: Key(catalog.id.toString()),
-              child: Image.network(catalog.image),
-            ).h32(context),
-            Expanded(
-                child: VxArc(
-              height: 30.0,
-              arcType: VxArcType.CONVEY,
-              edge: VxEdge.TOP,
-              child: Container(
-                color: Colors.white,
-                width: context.screenWidth,
-                child: Column(
-                  children: [
-                    catalog.name.text.xl4
-                        .color(MyTheme.darkBluishColor)
-                        .bold
-                        .make(),
-                    catalog.desc.text.textStyle(context.captionStyle).xl.make(),
-                    10.heightBox,
-                  ],
-                ).py64(),
-              ),
-            )),
-          ],
-        ).p16(),
-      ),
+          bottom: false,
+          child: Column(
+            children: [
+              Hero(
+                tag: Key(catalog.id.toString()),
+                child: Image.network(catalog.image),
+              ).h32(context),
+              Expanded(
+                  child: VxArc(
+                height: 30.0,
+                arcType: VxArcType.CONVEY,
+                edge: VxEdge.TOP,
+                child: Container(
+                  color: Colors.white,
+                  width: context.screenWidth,
+                  child: Column(
+                    children: [
+                      catalog.name.text.xl4
+                          .color(MyTheme.darkBluishColor)
+                          .bold
+                          .make(),
+                      catalog.desc.text
+                          .textStyle(context.captionStyle)
+                          .xl
+                          .make(),
+                      10.heightBox,
+                      "Eu do ea in non Lorem sit Lorem consectetur proident non irure voluptate pariatur est.Voluptate anim voluptate dolor excepteur laboris. Voluptate consequat eiusmod tempor excepteur laborum id fugiat dolore aute sunt commodo minim minim aute. "
+                          .text
+                          .textStyle(context.captionStyle)
+                          .make()
+                          .p16()
+                    ],
+                  ).py64(),
+                ),
+              )),
+            ],
+          )),
     );
   }
 }
